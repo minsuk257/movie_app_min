4536d5cb104ddc9010fa4e4a536f2a3ac1f31c9f
minsuk257 <hinez@naver.com>
2020-06-16 14:40:07 +0900

    Updates

---
+++ precache-manifest.e134bb7a2ca16a12c8ad286f8b94c257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f815ff05ac430154e30aaeb82e0bed",
+    "url": "/movie_app_min/index.html"
+  },
+  {
+    "revision": "5809898c62a61eb7c5d4",
+    "url": "/movie_app_min/static/css/main.44bd04b2.chunk.css"
+  },
+  {
+    "revision": "c770bf010c930118fe62",
+    "url": "/movie_app_min/static/js/2.9efe54e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_min/static/js/2.9efe54e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5809898c62a61eb7c5d4",
+    "url": "/movie_app_min/static/js/main.9579a7a5.chunk.js"
+  },
+  {
+    "revision": "3d49cc9ac308d90976ed",
+    "url": "/movie_app_min/static/js/runtime-main.17b14129.js"
+  }
+]);
